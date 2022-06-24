704933cdc97b525927d01b79e27eb91525a52695
dilasedetmen <sedetmendila@gmail.com>
2022-06-25 00:07:27 +0200

    js updated

--- script.js
+++ script.js
@@ -1,56 +1,60 @@
-// Gameboard as module
-(function () {
-  var Gameboard = {
-    gameboard: ["", "", "", "", "", "", "", "", ""],
-    init: function () {
-      this.cacheDOM();
-      this.createBoard();
-      this.render();
-      this.putSign();
-    },
-    cacheDOM: function () {
-      this.board = document.getElementById("gameboard");
-      this.cells = document.getElementsByClassName("board_cells");
-    },
-    createBoard: function () {
-      for (let i = 0; i <= 9; i++) {
-        const cell = document.createElement("div");
-        this.board.append(cell);
-        cell.className = "board_cells";
-        cell.id = i;
-      }
-    },
-    render: function () {
-      for (const [index, value] of this.gameboard.entries()) {
-        {
-          this.cells[index].innerHTML = `<p>${value}</p>`;
-        }
-      }
-    },
-    putSign: function () {
-      for (const cell of this.cells) {
-        cell.addEventListener("click", () => {
-          console.log(cell.id); //delete later
-          this.gameboard[cell.id] = player1.sign;
-          this.render();
-        });
-      }
-    },
-  };
-
-  Gameboard.init();
-})();
-
 //create players with Factory function
-const Player = (sign) => {
+const Player = (sign, isActive) => {
   const getSign = function () {
-    return { sign };
+    return sign;
   };
-
-  return { getSign };
+  return { getSign, isActive };
 };
 
-const player1 = Player("X");
-const player2 = Player("O");
-player1.getSign();
-player2.getSign();
+// Gameboard as module
+const Gameboard = (() => {
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+  const board = document.getElementById("gameboard");
+  const cells = document.getElementsByClassName("board_cells");
+
+  const createBoard = () => {
+    for (let i = 0; i <= 9; i++) {
+      const cell = document.createElement("div");
+      board.append(cell);
+      cell.className = "board_cells";
+      cell.id = i;
+    }
+  };
+  const render = () => {
+    for (const [index, value] of gameboard.entries()) {
+      {
+        cells[index].innerHTML = `<p>${value}</p>`;
+      }
+    }
+  };
+  return { gameboard, createBoard, render };
+})();
+
+//displayController as module
+const displayController = (() => {
+  Gameboard.createBoard();
+
+  const cells = document.getElementsByClassName("board_cells");
+
+  const putSign = (player, pStatus) => {
+    for (const cell of cells) {
+      cell.addEventListener("click", () => {
+        if (pStatus == true) {
+          Gameboard.gameboard[cell.id] = player.getSign();
+          Gameboard.render();
+          pStatus = false;
+        } else {
+          pStatus = true;
+        }
+      });
+    }
+  };
+  return { putSign };
+})();
+
+const player1 = Player("X", true);
+const player2 = Player("0", false);
+displayController.putSign(player1, true);
+displayController.putSign(player2, false);
+
+//gamelogic needed to stop overwriting on the gameboard
